ee1ce0780dd8cd13c7eef00de5c3f3b0b1e6863f
julio <julio@DESKTOP-K91BQGF.localdomain>
2021-11-24 01:25:58 +0200

    update v1

--- src/components/CreateWeapon.jsx
+++ src/components/CreateWeapon.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 const imagenames = ["gun1", "gun2", "gun3", "gun4", "gun5", "gun6"];
-const images = importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
+// const images = importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 
 function CreateWeapon() {
-  console.log(images);
   return imagenames.map((gun) => {
-    return <img className="weapon" key={gun} src={images[gun + ".png"].default} alt=""></img>;
+    return <img className="weapon" key={gun} src={require(gun + ".png")} alt=""></img>;
   });
 }
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    return (images[item.replace("./", "")] = r(item));
-  });
-  return images;
-}
+// function importAll(r) {
+//   let images = {};
+//   r.keys().map((item, index) => {
+//     return (images[item.replace("./", "")] = r(item));
+//   });
+//   return images;
+// }
 export default CreateWeapon;
+// images[gun + ".png"].default
