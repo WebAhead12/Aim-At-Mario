de19ac7436cc7380ec934a37cba92f1ab5fc525f
Mario Saliba <mario.saliba98@gmail.com>
2021-11-24 01:24:45 +0200

    5od

--- src/components/CreateWeapon.jsx
+++ src/components/CreateWeapon.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-const imagenames = ["gun1", "gun2", "gun3", "gun4", "gun5", "gun6"];
-const images = importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
+const imagenames = ["gun1.png", "gun2.png", "gun3.png", "gun4.png", "gun5.png", "gun6.png"];
+// const images = importAll(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 
 function CreateWeapon() {
-  console.log(images);
+  // console.log(images);
   return imagenames.map((gun) => {
-    return <img className="weapon" key={gun} src={images[gun + ".png"].default} alt=""></img>;
+    return <img className="weapon" key={gun} src={`images/${gun}`} alt=""></img>;
   });
 }
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    return (images[item.replace("./", "")] = r(item));
-  });
-  return images;
-}
+// function importAll(r) {
+//   let images = {};
+//   r.keys().map((item, index) => {
+//     return (images[item.replace("./", "")] = r(item));
+//   });
+//   return images;
+// }
 export default CreateWeapon;
